c7250bd6b858b7715884df1c5b16f82639a4df27
balogun14 <awwalbalogun06@gmail.com>
2023-09-08 22:22:31 +0100

    Added unit test

--- blog/tests.py
+++ blog/tests.py
@@ -1,2 +1,37 @@
 from django.test import TestCase
 
+from django.contrib.auth import get_user_model
+
+# Create your tests here.
+
+from .models import Post
+
+
+class BlogTests(TestCase):
+    """
+    This is to test the blog application
+    """
+
+    @classmethod
+    def setUpTestData(cls):
+        """
+        This sets up the custrm user for testing and also a new test db
+        which gets deleted afterwards.
+        """
+        cls.user = get_user_model().objects.create_user(
+            username="testuser", email="test@email.com", password="secret"
+        )
+
+        cls.post = Post.objects.create(
+            title="A good title", body="nice content", author=cls.user
+        )
+
+    def test_post_model(self):
+        """
+        This test if the contents are equal in short terms
+        """
+        self.assertEqual(self.post.title, "A good title")
+        self.assertEqual(self.post.body, "nice content")
+        self.assertEqual(self.post.author.username, "testuser")
+        self.assertEqual(str(self.post), "A good title")
+        self.assertEqual(self.post.get_absolute_url(), "/post/1")
